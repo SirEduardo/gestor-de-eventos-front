875b57c492df195ed6ca21d5fc0221844892548b
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-05 17:54:12 +0200

    Update loginForm.js

--- front/src/components/LoginForm/loginForm.js
+++ front/src/components/LoginForm/loginForm.js
@@ -6,8 +6,8 @@ export const LoginForm = (form) => {
     form.className = "login-form"
 
     form.innerHTML = `
-    ${FieldForm({ labelText: "Email", type: "email" })}
-    ${FieldForm({ labelText: "Password", type: "password" })}
+    ${FieldForm({ labelText: "Email", type: "email", id: "email" })}
+    ${FieldForm({ labelText: "Password", type: "password", id: "password" })}
     `
     form.append(Button({ text: "Login", fnc: () => {
 
