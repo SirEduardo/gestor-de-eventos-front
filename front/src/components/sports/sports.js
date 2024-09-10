538299dd202daddd510682eca2a99b867ad9d7db
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-10 13:32:41 +0200

    Update sports.js

--- front/src/components/sports/sports.js
+++ front/src/components/sports/sports.js
@@ -1,1 +1,23 @@
-export const sports = ["Fútbol", "Baloncesto", "Tenis", "Running", "Calistenia"];
+export const sports = [
+    {
+        name: "Fútbol", 
+        image: "https://s3.abcstatics.com/media/bienestar/2019/09/17/futbol-1-kU3C--1248x698@abc.jpg"
+    },
+    {
+        name: "Baloncesto",
+        image: "https://encage-cm.es/wp-content/uploads/2023/03/vista-silueta-jugador-baloncesto-baloncesto-pared-negra.jpg"
+    }, 
+    {
+        name: "Tenis",
+        image: "https://s3.abcstatics.com/media/bienestar/2019/08/02/tenis-abecedario-kgNF--1248x698@abc.jpg"
+
+    },
+    {
+        name: "Running",
+        image: "https://www.iberdrola.com/documents/20125/39940/running-mas-que-moda-746x419.jpg/40c26194-0a55-beff-b5f2-7cc4ca7533ce?t=1704699183011"
+    }, 
+    {
+        name: "Calistenia",
+        image: "https://sportmadness.club/wp-content/smush-webp/2023/09/calistenia.png.webp"
+    }
+];
